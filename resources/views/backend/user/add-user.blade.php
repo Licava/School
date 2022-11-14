29bc03d2952e7ad3a7ec206f38575035cd48657d
Licava Matining <tiltedmarshinn@gmail.com>
2022-11-14 10:44:52 +0800

    mga tang@

--- resources/views/backend/user/add-user.blade.php
+++ resources/views/backend/user/add-user.blade.php
@@ -12,7 +12,7 @@
             <div class="card">
             <div class="card-header">
                 <h5 class="card-title">
-                    Add User
+                    Add User mga tangas
                 </h5>
             </div>
             <div class="card-body">
