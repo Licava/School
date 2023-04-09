e0ae5993e3203c4a56383e43ca8a3893bb7c94b4
Licava <102903504+Licava@users.noreply.github.com>
2023-04-09 15:07:05 +0800

    unang commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,150 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Log in</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+<title>Preskool - Login</title>
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{asset('backend/plugins/fontawesome-free/css/all.min.css')}}">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="{{asset('backend/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{asset('backend/dist/css/adminlte.min.css')}}">
-  <!-- Toaster Notification -->
-<link rel="stylesheet" href="{{('toaster/toastr.min.css')}}">
+<link rel="shortcut icon" href="{{('design2/img/favicon.png')}}">
 
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="{{('design2/plugins/bootstrap/css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="{{('design2/plugins/feather/feather.css')}}">
+
+<link rel="stylesheet" href="{{('design2/plugins/icons/flags/flags.css')}}">
+
+<link rel="stylesheet" href="{{('design2/plugins/fontawesome/css/fontawesome.min.css')}}">
+<link rel="stylesheet" href="{{('design2/plugins/fontawesome/css/all.min.css')}}">
+
+<link rel="stylesheet" href="{{('design2/css/stylee.css')}}">
 </head>
-<body class="hold-transition login-page">
-<div class="login-box">
-  <div class="login-logo">
-  
-  </div>
-  <!-- /.login-logo -->
-  <div class="card" >
-    <div class="card-body login-card-body">
-      <p class="login-box-msg">Scholarship Management System</p>
-      @if(count($errors))
-  @foreach ($errors->all() as $error)
-  <p class="alert alert-danger alert-dismissible fade show">{{$error}}</p>
-  @endforeach
-  @endif
-      <form method="POST" action="{{ route('login') }}">
-                        @csrf
-                
-        <div class="input-group mb-3">
-        
-          <input id="email" type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-          
+<body>
 
-        
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
-            </div>
-          </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" placeholder="Password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" id="password" >
-       
-          <div class="input-group-append">
-            <div class="input-group-text">
-     
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Remember Me
-              </label>
-            </div>
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-
-      <div class="social-auth-links text-center mb-3">
-        <p>- OR -</p>
-        <a href="#" class="btn btn-block btn-success">
-          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-        </a>
-      </div>
-      <!-- /.social-auth-links -->
-
-      <p class="mb-1">
-       <center> <a href="{{ route('password.request') }}">I forgot my password</a> </center>
-      </p>
-      <p class="mb-0">
-       <center> <a href="{{ route('register') }}" class="text-center">Register a new membership</a></center>
-      </p>
-    </div>
-    <!-- /.login-card-body -->
-  </div>
+<div class="main-wrapper login-body">
+<div class="login-wrapper">
+<div class="container">
+<div class="loginbox">
+<div class="login-left">
+<img class="img-fluid" src="{{('design2/img/login.png')}}" alt="Logo">
 </div>
-<!-- /.login-box -->
-<!-- Start Toaster & Sweetalert -->
-<script src="{{('toaster/toastr.min.js')}}"></script>
-<script src="{{('toaster/sweetalert.min.js') }}"></script>
+<div class="login-right">
+<div class="login-right-wrap">
+<h1>Welcome to Preskool</h1>
+<p class="account-subtitle">Need an account? <a href="{{ route('register') }}">Sign Up</a></p>
+<h2>Sign in</h2>
+
+<form method="POST" action="{{ route('login') }}">
+                        @csrf
+<div class="form-group">
+<label>Username <span class="login-danger">*</span></label>
+<input id="email" type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+<span class="profile-views"><i class="fas fa-user-circle"></i></span>
+</div>
+<div class="form-group">
+<label>Password <span class="login-danger">*</span></label>
+<input type="password" placeholder="Password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" id="password" >
+<span class="profile-views feather-eye toggle-password"></span>
+</div>
+<div class="forgotpass">
+<div class="remember-me">
+<label class="custom_check mr-2 mb-0 d-inline-flex remember-me"> Remember me
+<input type="checkbox" name="radio">
+<span class="checkmark"></span>
+</label>
+</div>
+<a href="{{ route('password.request') }}">Forgot Password?</a>
+</div>
+<div class="form-group">
+<button class="btn btn-primary btn-block" type="submit">Login</button>
+</div>
+</form>
+
+<div class="login-or">
+<span class="or-line"></span>
+<span class="span-or">or</span>
+</div>
+
+<div class="social-login">
+<a href="#"><i class="fab fa-google-plus-g"></i></a>
+<a href="#"><i class="fab fa-facebook-f"></i></a>
+<a href="#"><i class="fab fa-twitter"></i></a>
+<a href="#"><i class="fab fa-linkedin-in"></i></a>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
 
 
-<script>
-            @if(Session::has('messege'))
-              var type="{{Session::get('alert-type','info')}}"
-              switch(type){
-                  case 'info':
-                       toastr.info("{{ Session::get('messege') }}");
-                       break;
-                  case 'success':
-                      toastr.success("{{ Session::get('messege') }}");
-                      break;
-                  case 'warning':
-                     toastr.warning("{{ Session::get('messege') }}");
-                      break;
-                  case 'error':
-                      toastr.error("{{ Session::get('messege') }}");
-                      break;
-              }
-            @endif
-         </script>  
+<script src="{{('design2/js/jquery-3.6.0.min.js')}}"></script>
 
+<script src="{{('design2/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
-<script>  
-             $(document).on("click", "#delete", function(e){
-                 e.preventDefault();
-                 var link = $(this).attr("href");
-                    swal({
-                      title: "Are you Want to delete?",
-                      text: "Once Delete, This will be Permanently Delete!",
-                      icon: "warning",
-                      buttons: true,
-                      dangerMode: true,
-                    })
-                    .then((willDelete) => {
-                      if (willDelete) {
-                           window.location.href = link;
-                      } else {
-                        swal("The Data has been saved !");
-                      }
-                    });
-                });
-        </script>
-       
-<!-- End Toaster & Sweetalert -->
-<!-- jQuery -->
-<script src="{{asset('backend/plugins/jquery/jquery.min.js')}}"></script>
-<!-- Bootstrap 4 -->
-<script src="{{asset('backend/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<!-- AdminLTE App -->
-<script src="{{asset('backend/dist/js/adminlte.min.js"></script>
+<script src="{{('design2/js/feather.min.js')}}"></script>
+
+<script src="{{('design2/js/script.js')}}"></script>
 </body>
 </html>
